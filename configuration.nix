7133cf8825066010b7e8aadefd5a15620834c4da
ClemTheAlien <ClemTheAlien@proton.me>
2025-05-18 21:40:38 -0500

    Decleratively configured zsh

--- configuration.nix
+++ configuration.nix
@@ -55,7 +55,9 @@
 	};
 	#Default Shell (ZSH)
 	programs.zsh.enable = true; # Use script with dotfiles to install zsh plugins
-		
+	programs.zsh.autosuggestions.enable = true;
+  programs.zsh.syntaxHighlighting.enable = true;
+
 	# Define a user account.
 	users.users.clemmie = {
 		isNormalUser = true;
