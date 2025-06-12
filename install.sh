432b77d236e6ce92a1ffa450a78fcb94c71e9bb6
ClemTheAlien <ClemTheAlien@proton.me>
2025-06-11 19:54:28 -0500

    Hope this works

--- install.sh
+++ install.sh
@@ -3,21 +3,20 @@ if [[ $(/usr/bin/id -u) -ne 0 ]]; then
     exit
 fi
 
-rm -rf ~/nixos_config/assets
-rm ~/nixos_config/README.md
-cp -r ~/nixos_config/.zshrc ~
-cp -r ~/nixos_config/extraConfig ~/.extraConfig
-cp -r ~/nixos_config/configuration.nix /etc/nixos/
-cp -r ~/nixos_config/* ~/.config/
+echo Hello, what is ur username?
+read user
 
+rm -rf /home/$user/nixos_config/assets
+rm /home/$user/nixos_config/README.md
+cp -r /home/$user/nixos_config/.zshrc ~
+rm .zshrc
+cp -r /home/$user/nixos_config/extraConfig ~/.extraConfig
+rm -rf /home/$user/nixos_config/extraConfig
+cp -r /home/$user/nixos_config/configuration.nix /etc/nixos/
+rm /home/$user/nixos_config/configuration.nix
+cp -r /home/$user/nixos_config/* ~/.config/
 
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub com.valvesoftware.Steam
-flatpak install flathub org.prismlauncher.PrismLauncher
-flatpak install flathub com.spotify.Client
-flatpak install flathub com.github.tchx84.Flatseal
-
-rm -rf ~/nixos_config
+rm -rf /home/$user/nixos_config
 nixos-rebuild boot --install-bootloader
 
 echo "All Done :3"
