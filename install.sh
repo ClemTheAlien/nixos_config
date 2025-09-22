3bf45a7078069df5c11a9bdf4ac03f2201e44110
ClemTheAlien <ClemTheAlien@proton.me>
2025-09-22 16:35:55 -0500

    Added Home Manager Flake :3

--- install.sh
+++ install.sh
@@ -68,6 +68,7 @@ rm -r ~/.nixos_config
 mkdir ~/.nixos_config
 cp home.nix ~/.nixos_config
 cp configuration.nix ~/.nixos_config
+cp flake.nix ~/.nixos_config
 cp .zshrc ~/.zshrc
 cp -r ./modules ~/.nixos_config/modules
 sleep 3
