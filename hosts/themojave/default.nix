265afa4bab48230eb28c15fa2088cfd0eb516b2f
ClemTheAlien <ClemTheAlien@proton.me>
2026-05-15 11:19:09 -0500

    podman and distrobox

--- hosts/themojave/default.nix
+++ hosts/themojave/default.nix
@@ -6,6 +6,7 @@
     ../../modules/core
     ../../modules/desktop
     ../../modules/apps
+    ../../modules/core/virtualization
   ];
   networking.hostName = "themojave";
 }
--- modules/core/virtualization/default.nix
+++ modules/core/virtualization/default.nix
@@ -5,4 +5,18 @@
 	virtualisation.libvirtd.enable = true;
 	#Docker
 	virtualisation.docker.enable = true;
+	#Podman
+	virtualisation.containers.enable = true;
+	virtualisation.podman = {
+  enable = true;
+};
+	#Distrobox
+	environment.systemPackages = [
+  pkgs.distrobox
+];
+
+# Ensure Distrobox can resolve symlinks pointing back to the Nix store
+environment.etc."distrobox/distrobox.conf".text = ''
+  container_additional_volumes="/nix/store:/nix/store:ro"
+'';
     }
